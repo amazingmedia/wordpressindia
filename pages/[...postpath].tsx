ae77e6cad7da179521f284eadb9df6454c964e18
amazingmedia <131604200+amazingmedia@users.noreply.github.com>
2023-11-21 16:46:44 +0630

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -7,7 +7,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 	const endpoint = "https://ahkalex.com/graphql";
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
-	const pathArr = ctx.query.postpath as Array<string>;
+	const pathArr = ctx.query.pagepath as Array<string>;
 	const path = pathArr.join('/');
 	console.log(path);
 	const fbclid = ctx.query.fbclid;
